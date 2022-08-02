157aed4d4488e8478d00e0c85f054d377251084b
Reljod <reljod.oreta@paymaya.com>
2022-08-02 12:54:54 +0800

    feat: implement java cats cli with cat of the day

---
+++ src/App.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to Java Cats!");
+        Cat cat = new Cat();
+            
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Type 'enter' or 'Y' to continue. 'N' to exit.");
+            String answerStr = scanner.nextLine().toLowerCase();
+            if (answerStr.equals("n")) {
+                System.out.println("Thank you for using Java Cats! Goodbye!");
+                return;
+            }
+            System.out.println("Do you want to see the Cat of the day? Type 'enter' or 'Y' if yes. 'N' to continue.");
+            answerStr = scanner.nextLine().toLowerCase();
+            if (answerStr.equals("y") || answerStr.equals("")) {
+                System.out.println("The cat of the day says '" + cat.wordOfTheDay + "'");
+                cat.getCatOfTheDay();
+                return;
+            }
+        }
+    }
+}
+
+
---
+++ src/Cat.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.awt.FlowLayout;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Cat {
+
+	private final static String CAT_URL = "https://cataas.com/cat/says/";
+
+	private String[] randomTexts = { "Hello", "Hi", "Sup bro", "Sheesh", "Yo wazzup", "Carps carps?" };
+
+	public String wordOfTheDay;
+
+	public Cat() {
+		this.wordOfTheDay = getRandomText();
+	}
+
+	private String getRandomText() {
+		return randomTexts[(int) Math.floor(Math.random() * randomTexts.length)];
+	}
+
+	public String getCatOfTheDay() throws MalformedURLException {
+		URL url = new URL(CAT_URL + this.wordOfTheDay);
+		try {
+			var c = ImageIO.read(url);
+			ImageIcon image = new ImageIcon(c);
+			JFrame frame = new JFrame();
+			frame.setLayout(new FlowLayout());
+			frame.setSize(800,800);
+
+			JLabel lbl = new JLabel();
+			lbl.setIcon(image);
+			frame.add(lbl);
+			frame.setVisible(true);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return "Meowth" + " " + this.getRandomText();
+	}
+}
